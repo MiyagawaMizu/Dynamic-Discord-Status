b7478b11862307e21d91cc42f2e270fcb3932eb7
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2023-09-23 19:21:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-const userID = "738748102311280681"; //put your Discord user id here
+const userID = "906037896295878706"; //put your Discord user id here
 const statusImage = document.getElementById("status-image");
 
 async function fetchDiscordStatus() {
-  try {
-    const response = await axios.get(
-      `https://api.lanyard.rest/v1/users/${userID}`
-    );
-    const { data } = response.data;
-    const { discord_status, activities } = data;
+	try {
+		const response = await axios.get(
+			`https://api.lanyard.rest/v1/users/${userID}`
+		);
+		const { data } = response.data;
+		const { discord_status, activities } = data;
 
-    // Get the image path corresponding to the state
-    let imagePath;
-    switch (discord_status) {
-      case "online":
-        imagePath = "./status/online.svg";
-        break;
-      case "idle":
-        imagePath = "./status/idle.svg";
-        break;
-      case "dnd":
-        imagePath = "./status/dnd.svg";
-        break;
-      case "offline":
-        imagePath = "./status/offline.svg";
-        break;
-      default:
-        imagePath = "";
-        break;
-    }
+		// Get the image path corresponding to the state
+		let imagePath;
+		switch (discord_status) {
+			case "online":
+				imagePath = "./status/online.svg";
+				break;
+			case "idle":
+				imagePath = "./status/idle.svg";
+				break;
+			case "dnd":
+				imagePath = "./status/dnd.svg";
+				break;
+			case "offline":
+				imagePath = "./status/offline.svg";
+				break;
+			default:
+				imagePath = "";
+				break;
+		}
 
-    // Check operation status to update image path
-    if (
-      activities.find(
-        (activity) => activity.type === 1 && activity.url.includes("twitch.tv")
-      )
-    ) {
-      imagePath = "/status/streaming.svg";
-    }
+		// Check operation status to update image path
+		if (
+			activities.find(
+				(activity) =>
+					activity.type === 1 &&
+					(activity.url.includes("twitch.tv") ||
+						activity.url.includes("youtube.com"))
+			)
+		) {
+			imagePath = "/public/status/streaming.svg";
+		}
 
-    // Update images
-    statusImage.src = imagePath;
-    statusImage.alt = `Discord Status: ${discord_status}`;
-  } catch (error) {
-    console.error("Unable to get Discord status:", error);
-  }
+		// Update images
+		statusImage.src = imagePath;
+		statusImage.alt = `Discord Status: ${discord_status}`;
+	} catch (error) {
+		console.error("Unable to get Discord status:", error);
+	}
 }
 
 fetchDiscordStatus();
